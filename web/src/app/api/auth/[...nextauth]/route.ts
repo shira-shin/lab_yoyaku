73ea1c57b546422bc6d5993fdfb2b2bb1387adba
shira-shin <shiratorien9837@gmail.com>
2025-10-13 02:41:31 +0900

    Fix NextAuth configuration for Prisma and Google

--- web/src/app/api/auth/[...nextauth]/route.ts
+++ web/src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,18 @@
-import { handlers } from "@/auth";
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/lib/prisma";
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  trustHost: true,
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    Google({
+      // allowDangerousEmailAccountLinking: true,
+    }),
+  ],
+  // session: { strategy: "database" },
+  // debug: process.env.NODE_ENV !== "production",
+});
 
 export const { GET, POST } = handlers;
