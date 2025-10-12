b006a1718a359d0b644b62c049e55fb4cf69d40c
shira-shin <shiratorien9837@gmail.com>
2025-10-13 03:27:09 +0900

    Refactor NextAuth route handlers

--- web/src/app/api/auth/[...nextauth]/route.ts
+++ web/src/app/api/auth/[...nextauth]/route.ts
@@ -1,19 +1,3 @@
-import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-
-import { prisma } from "@/server/prisma";
-
-export const { handlers, auth, signIn, signOut } = NextAuth({
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    Google({
-      clientId: process.env.AUTH_GOOGLE_ID!,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
-    }),
-  ],
-  session: { strategy: "database" },
-  secret: process.env.AUTH_SECRET,
-});
+import { handlers } from "@/auth";
 
 export const { GET, POST } = handlers;
--- web/src/auth.ts
+++ web/src/auth.ts
@@ -1,1 +1,17 @@
-export { handlers, auth, signIn, signOut } from "@/app/api/auth/[...nextauth]/route";
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+
+import { prisma } from "@/lib/prisma";
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID!,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+    }),
+  ],
+  session: { strategy: "jwt" },
+  secret: process.env.AUTH_SECRET,
+});
