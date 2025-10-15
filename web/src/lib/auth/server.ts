3cfdb4eb9698e26f89b4644389f39bc789458dba
shira-shin <shiratorien9837@gmail.com>
2025-10-16 03:51:16 +0900

    chore(auth): rollback to next-auth@4.24.7 and refresh lockfile

--- web/src/lib/auth/server.ts
+++ web/src/lib/auth/server.ts
@@ -1,7 +1,12 @@
 import "server-only";
 
-export { auth } from "@/auth";
-export { auth as getServerSession } from "@/auth";
+import { getServerSession as nextGetServerSession } from "next-auth/next";
+
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+
+export const auth = () => nextGetServerSession(authOptions);
+export const getServerSession = () => nextGetServerSession(authOptions);
+export const getSessionServer = () => nextGetServerSession(authOptions);
 
 import { readUserFromCookie as _readUserFromCookie } from "../auth-legacy";
 export { readUserFromCookie } from "../auth-legacy";
