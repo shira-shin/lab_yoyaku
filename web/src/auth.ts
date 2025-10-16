23787d9fca19387349716fe83a5466a76814111c
shira-shin <shiratorien9837@gmail.com>
2025-10-17 05:17:13 +0900

    Fix NextAuth route exports

--- web/src/auth.ts
+++ web/src/auth.ts
@@ -1,7 +1,7 @@
-import NextAuth from "next-auth";
+import NextAuth, { type NextAuthConfig } from "next-auth";
 import Google from "next-auth/providers/google";
 
-const nextAuth = NextAuth({
+export const authConfig = {
   providers: [
     Google({
       clientId:
@@ -12,9 +12,6 @@ const nextAuth = NextAuth({
   ],
   trustHost: true,
   secret: process.env.AUTH_SECRET ?? process.env.NEXTAUTH_SECRET,
-});
+} satisfies NextAuthConfig;
 
-export const { auth, signIn, signOut } = nextAuth;
-
-export const GET = nextAuth.handlers.GET;
-export const POST = nextAuth.handlers.POST;
+export const { auth, signIn, signOut } = NextAuth(authConfig);
