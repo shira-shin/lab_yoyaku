7829cf73ad72a56799fe0c54b7746e0fa36fda4b
shira-shin <shiratorien9837@gmail.com>
2025-10-17 11:24:45 +0900

    Refactor NextAuth configuration for v5

---
+++ web/src/auth.config.ts
@@ -0,0 +1,7 @@
+import type { NextAuthConfig } from "next-auth";
+import Google from "next-auth/providers/google";
+
+export const authConfig: NextAuthConfig = {
+  providers: [Google],
+  trustHost: true,
+};
