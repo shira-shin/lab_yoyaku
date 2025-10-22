b868fff419e9626443b427294eab7948fb7f7840
shira-shin <shiratorien9837@gmail.com>
2025-10-22 12:36:41 +0900

    Fix NextAuth route auth import path

--- web/src/app/api/auth/[...nextauth]/route.ts
+++ web/src/app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,2 @@
-export { handlers as GET, handlers as POST } from "../../../../../../auth";
+export { handlers as GET, handlers as POST } from "../../../../../auth";
 export const runtime = "nodejs";
