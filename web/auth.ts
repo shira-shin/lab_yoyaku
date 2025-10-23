e98b02a6e0569e2e92f4447793d90e79ad122af5
shira-shin <shiratorien9837@gmail.com>
2025-10-24 02:51:57 +0900

    Call Google provider with OAuth credentials

--- web/auth.ts
+++ web/auth.ts
@@ -4,13 +4,13 @@ import Google from "next-auth/providers/google";
 export const authConfig: NextAuthConfig = {
   trustHost: true,
   session: { strategy: "jwt" },
+  secret: process.env.AUTH_SECRET,
   providers: [
     Google({
       clientId: process.env.GOOGLE_OAUTH_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_OAUTH_CLIENT_SECRET!,
     }),
   ],
-  secret: process.env.AUTH_SECRET,
 };
 
 export const { handlers, auth, signIn, signOut } = NextAuth(authConfig);
