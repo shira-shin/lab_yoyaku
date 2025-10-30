30a6076ea4aa9ef9d4931a90e5d93c6e2a0a9474
shira-shin <shiratorien9837@gmail.com>
2025-10-31 00:55:14 +0900

    Ensure Prisma client is generated during build

--- web/src/lib/prisma.ts
+++ web/src/lib/prisma.ts
@@ -1,1 +1,13 @@
-export { prisma } from '@/server/db/prisma'
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    // log: ["query", "info", "warn", "error"],
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
